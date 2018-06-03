094e44bd18c7784653b54b1598119758ef74c437
Mohammed b. Terry b. Jack <32687826+mohammedterry@users.noreply.github.com>
2018-06-03 01:45:43 +0100

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -4,7 +4,7 @@ from keras.layers import Dense, Activation, LSTM
 from keras.optimizers import RMSprop
 
 def cleaned_text(text):
-    return ' '.join(''.join([ch.lower() for ch in text if 64 < ord(ch) < 91 or 96 < ord(ch) < 123 or ch in ' !,.:;?']).split())
+    return ' '.join(''.join([ch.lower() for ch in text if 31 < ord(ch) < 128]).split())
 
 def window_transform_text(text, window_size=100, step_size=5):
     inputs = [text[i:i+window_size] for i in range(0,len(text)-window_size,step_size)]
@@ -19,8 +19,8 @@ def encode_io_pairs(text,window_size=100,step_size=5):
     y = np.zeros((len(inputs), num_chars), dtype=np.bool)
     for i, sentence in enumerate(inputs):
         for t, char in enumerate(sentence):
-            X[i, t, chars_to_indices[char]] = 1
-        y[i, chars_to_indices[outputs[i]]] = 1     
+            X[i, t, chars_to_indices(char)] = 1
+        y[i, chars_to_indices(outputs[i])] = 1     
     return X,y
 
 def predict_next_chars(model,input_chars,num_to_predict):     
@@ -28,26 +28,28 @@ def predict_next_chars(model,input_chars,num_to_predict):
     for i in range(num_to_predict):
         x_test = np.zeros((1, window_size, len(chars)))
         for t, char in enumerate(input_chars):
-            x_test[0, t, chars_to_indices[char]] = 1.
+            x_test[0, t, chars_to_indices(char)] = 1.
         test_predict = model.predict(x_test,verbose = 0)[0]
         r = np.argmax(test_predict)                           
-        d = indices_to_chars[r] 
+        d = indices_to_chars(r) 
         predicted_chars+=d
         input_chars+=d
         input_chars = input_chars[1:]
     return predicted_chars
 
+def indices_to_chars(i):
+    return chr(i + 32)
+
+def chars_to_indices(ch):
+    return ord(ch) - 32
+
 # clean input text
 text = cleaned_text(open('twitter.txt').read())
-chars = set(text)
-print ("\n{} ...\n\nthis corpus has: \n\t{} characters in total\n\t{} unique characters\n\t\t{}".format(text[:100],len(text),len(chars),chars))
-# convert chars to nums
-chars_to_indices = dict((c, i) for i, c in enumerate(chars)) 
-indices_to_chars = dict((i, c) for i, c in enumerate(chars))  
+print ("\n{} ...\n\nthis corpus has: \n\t{} characters in total\n\t{} unique characters\n\t\t{}".format(text[:100],len(text),len(set(text)),set(text)))
 X,y = encode_io_pairs(text)
 # build train and save LSTM
 model = Sequential()
-model.add(LSTM(200,input_shape=(100,len(chars))))
+model.add(LSTM(200,input_shape=(100,len(set(text)))))
 model.add(Dense(num_chars))
 model.add(Activation("softmax"))
 model.compile(loss='categorical_crossentropy',optimizer=RMSprop(lr=.001,rho=.9,epsilon=1e-08,decay=.0))
